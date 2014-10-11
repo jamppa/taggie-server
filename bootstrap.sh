3277fed703409b3d7539ac1505a67c6cc1ee9dc4
Jani Arvonen <jani.arvonen@gmail.com>
2014-10-11 14:09:15 +0300

    moved Dockerfile to docker and use prebuilt docker image from docker hub when bootstrapping the dev environment

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-sudo docker build -t taggie-server .
-sudo docker run -t -i --name taggie-server-dev -v `pwd`:/home/taggie/dev -P taggie-server
+sudo docker run -t -i --name taggie_server_dev -v `pwd`:/home/taggie/dev -P jamppa/taggie_server
