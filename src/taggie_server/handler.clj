3e9c2be3d211f87c5b1638c0031ebbe8ea1da954
Jani Arvonen <jani.arvonen@gmail.com>
2014-10-16 20:11:23 +0300

    formatted

--- src/taggie_server/handler.clj
+++ src/taggie_server/handler.clj
@@ -1,20 +1,20 @@
 (ns taggie-server.handler
-	(:use
-		taggie-server.routes.subject-routes
-		taggie-server.routes.user-routes)
-	(:require 
-		[compojure.core :refer :all]
-		[compojure.handler :as handler]
-		[compojure.route :as route]
-		[ring.middleware.format-params :refer [wrap-restful-params]]
-		[ring.middleware.format-response :refer [wrap-restful-response]]))
+    (:use
+        taggie-server.routes.subject-routes
+        taggie-server.routes.user-routes)
+    (:require 
+        [compojure.core :refer :all]
+        [compojure.handler :as handler]
+        [compojure.route :as route]
+        [ring.middleware.format-params :refer [wrap-restful-params]]
+        [ring.middleware.format-response :refer [wrap-restful-response]]))
 
 (defroutes app-routes
-	(context "/api" [] subject-routes user-routes)
-	(route/not-found "Requested URL not found"))
+    (context "/api" [] subject-routes user-routes)
+    (route/not-found "Requested URL not found"))
 
 (def app
-	(->
-		(handler/site app-routes)
-		(wrap-restful-params)
-		(wrap-restful-response)))
+    (->
+        (handler/site app-routes)
+        (wrap-restful-params)
+        (wrap-restful-response)))
